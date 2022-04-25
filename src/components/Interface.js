473281dfc432d570fa5bf84a5834ff74026bbb00
galci <gantar.gal@gmail.com>
2022-04-25 17:23:13 +0200

    Data

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -4,17 +4,19 @@ import CanvasDraw from "react-canvas-draw";
 import { pedersen } from "../pedersen";
 
 
-function placeBet(canvasRef) {
+function placeBet(canvasRef, horseNum, bet) {
     let hash = require('object-hash')
-    let secret = hash(canvasRef.current.getSaveData())
+    let imageHash = hash(canvasRef.current.getSaveData())
+    let secret = parseInt(imageHash.substr(0, 10), 16);
     console.log("secret: ", secret)
     canvasRef.current.clear()
-    console.log("pedersen: ", pedersen([100, 0]));
+    let pedersenHash = pedersen([secret, 0])
+    console.log("pedersen: ", pedersenHash);
+    console.log("horsenum: ", horseNum, " bet ", bet);
 }
 
 export function Interface(props) {
     const [horseNum, setHorseNum] = useState('1');
-    const [hash, setHash] = useState("0x19fs232");
     const [bet, setBet] = useState("10");
 
     const canvasRef = createRef();
@@ -46,7 +48,7 @@ export function Interface(props) {
                 <Spacer h={.5} />
             </div>
 
-            <div className="interface-controls"><div className="button-wrapper"><Button className="bet-button" shadow type="secondary" scale={2}>Success</Button></div></div>
+            <div className="interface-controls"><div className="button-wrapper"><Button onClick={() => placeBet(canvasRef, horseNum, bet)} className="bet-button" shadow type="secondary" scale={2}>Success</Button></div></div>
         </div>
   );
 }
