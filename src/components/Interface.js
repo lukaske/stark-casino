9fbb6848a904d57cf0b0edffb7c435b1efc45fe6
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2022-04-25 17:08:09 +0200

    fixed button

--- src/components/Interface.js
+++ src/components/Interface.js
@@ -44,7 +44,7 @@ export function Interface(props) {
                 
                 <Spacer h={.5} />
             </div>
-            <Button className="bet-button" shadow type="secondary" scale={2}>Success</Button>
+            <div className="interface-controls"><div className="button-wrapper"><Button className="bet-button" shadow type="secondary" scale={2}>Success</Button></div></div>
         </div>
   );
 }
