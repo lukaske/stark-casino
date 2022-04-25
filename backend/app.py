ea8b58d74884085815e5f3e433014ae2c4174715
galci <gantar.gal@gmail.com>
2022-04-26 00:18:18 +0200

    Updated .gitignore

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 import flask
+from flask import request
 import datetime
 
 app = flask.Flask(__name__)
 
 time = False
+secrets_dict = {}
 
 @app.route("/time")
 def home():
@@ -13,3 +15,14 @@ def home():
     
     return { "time" : str(time) }
 
+@app.route("/secrets", methods = ['POST', 'GET'])
+def secrets():
+    global secrets_dict
+    if request.method == 'POST':
+        address = request.args.get('address', default = "", type = str)
+        secret = request.args.get('secret', default = "", type = str)
+
+        secrets_dict[address] = secret
+    
+        return secrets_dict
+    return { "code ": 404 }
